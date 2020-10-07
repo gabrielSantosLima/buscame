b12fa01ab3ade397efd21ce354781e07d1bc9d95
jonasjss <jonas.santos2302@gmail.com>
2020-10-07 01:47:27 -0400

    [ #2 ] Splash Screen, Tutorial and ic_launcher

--- app/src/main/java/com/app/buscame/TutorialThree.kt
+++ app/src/main/java/com/app/buscame/TutorialThree.kt
@@ -3,9 +3,9 @@ package com.app.buscame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TutorialThree : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.tutorial_three)
     }
 }
