9af09ecd03d381f877f878c16740d0518f3dcfef
Gabriel Lima <bielsantoslima10@gmail.com>
2020-10-20 19:23:46 -0400

    [#8] Finished the first step to message feature feature

--- app/src/main/java/com/app/buscame/TutorialOne.kt
+++ app/src/main/java/com/app/buscame/TutorialOne.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityOptionsCompat
+import com.app.buscame.dto.ProductDto
 import com.app.buscame.features.share.ShareMessage
 import kotlinx.android.synthetic.main.tutorial_one.*
+import java.io.File
 
 class TutorialOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +22,18 @@ class TutorialOne : AppCompatActivity() {
     }
 
     private fun algo(){
-        ShareMessage(this, "Teste").shareMessage("O site wwww.google.com.br é muito bom. Experimente!")
+        val productDto = ProductDto(
+            "ade34sdd4d",
+            "computador",
+            "Computador Muito Bom",
+            123.00,
+            "Computador vem com Windows 7, 350Gb de memória RAM (Sonho)",
+            "www.mercadolivre.com",
+            null
+        )
+
+        ShareMessage(this, "Teste")
+            .shareProduct(productDto)
     }
 
     private fun openNextActivity() {
