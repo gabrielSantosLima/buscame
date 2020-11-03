c967a68f2feb2cfa58aac4562f5745cd87f45cd9
SarahJ315 <61762426+SarahJ315@users.noreply.github.com>
2020-11-03 14:45:57 -0400

    Resolve conflicts

---
+++ app/src/main/java/com/app/buscame/HistoricActivity.kt
@@ -0,0 +1,11 @@
+package com.app.buscame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistoricActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historic)
+    }
+}
