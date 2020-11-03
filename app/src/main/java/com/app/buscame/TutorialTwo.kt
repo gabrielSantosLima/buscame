e85e21b486453144bb40fb8c01ef2927d47a269d
jonasjss <jonas.santos2302@gmail.com>
2020-11-02 22:13:29 -0400

    [ #11 ] SearchByText, Settings and About

--- app/src/main/java/com/app/buscame/TutorialTwo.kt
+++ app/src/main/java/com/app/buscame/TutorialTwo.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityOptionsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.tutorial_two.*
 
 class TutorialTwo : AppCompatActivity() {
@@ -12,15 +15,13 @@ class TutorialTwo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tutorial_two)
 
-        btnAdvance.setOnClickListener {
-            openNextActivity()
-        }
+        setupActionBarWithNavController(findNavController(R.id.fragmentTutorialTwo))
+
     }
 
-    private fun openNextActivity() {
-        val intent = Intent(this, TutorialThree::class.java)
-        val activityOptionsCompat = ActivityOptionsCompat.makeCustomAnimation(applicationContext, R.anim.move_left, R.anim.move_left)
-        ActivityCompat.startActivities(this, arrayOf(intent), activityOptionsCompat.toBundle())
+    override fun onSupportNavigateUp(): Boolean {
+        val NavController: NavController = findNavController(R.id.fragmentTutorialTwo)
+        return NavController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
