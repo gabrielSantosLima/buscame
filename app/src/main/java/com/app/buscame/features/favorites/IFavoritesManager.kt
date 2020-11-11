60f960d00975065a4ffbf2509da4c276a97d9e0f
Gabriel Lima <bielsantoslima10@gmail.com>
2020-11-11 16:15:21 -0400

    [#14] Added list and adapter favorites

--- app/src/main/java/com/app/buscame/features/favorites/IFavoritesManager.kt
+++ app/src/main/java/com/app/buscame/features/favorites/IFavoritesManager.kt
@@ -5,6 +5,7 @@ import com.app.buscame.dto.FavoriteDto
 interface IFavoritesManager {
     fun save(favorite: FavoriteDto)
     fun list(): List<FavoriteDto>
+    fun exists(id: String): Boolean
     fun remove(id : String)
     fun removeAll()
 }
