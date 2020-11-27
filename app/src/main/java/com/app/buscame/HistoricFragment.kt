8481a08c1e95b7e57bdded6604e32adeefaca12c
jonasjss <jonas.santos2302@gmail.com>
2020-11-27 09:28:33 -0400

    Navigation

--- app/src/main/java/com/app/buscame/HistoricFragment.kt
+++ app/src/main/java/com/app/buscame/HistoricFragment.kt
@@ -6,15 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.fragment_historic.*
 
-class AboutFragment : Fragment() {
+class HistoricFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        return inflater.inflate(R.layout.fragment_historic, container, false)
     }
-
 }
